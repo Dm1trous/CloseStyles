04b13ad94675efc0e45c7463ee27f0e6b1e78d0f
dm1trous <lopic9999lokoko@gmail.com>
2025-06-10 16:16:09 +0500

    last

--- SiteClothes/SiteClothes/settings.py
+++ SiteClothes/SiteClothes/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'cloudinary',
-    'django_cloudinary_storage',
+    'cloudinary_storage',
 ]
 
 CLOUDINARY_STORAGE = {
